3f6225e9b7bbc6e5cb201ab505e531bf5cb3ac2c
just-hms <alessandroversari1@gmail.com>
2024-02-28 23:11:35 +0100

    fix: main path

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -8,27 +8,28 @@ fi
 VERSION=$1
 BUILD="./build/"
 OUT="./out/"
+MAIN="./cmd/el/main.go"
 
 echo "Creating the build folders..."
 mkdir -p $OUT
 mkdir -p $BUILD
 
-echo "Copying shell files to build folder..."
+echo "Copying shell files to the build folder..."
 cp -r ./shell/. $BUILD
 
 echo "Building..."
 
 echo "linux"
-env GOARCH=amd64 GOOS=linux go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el ./cmd/
+env GOARCH=amd64 GOOS=linux go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el $MAIN
 tar -cvzf $OUT/el-linux-amd64.tar.gz -C $BUILD .
 
 echo "darwin"
-env GOARCH=amd64 GOOS=darwin go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el ./cmd/
+env GOARCH=amd64 GOOS=darwin go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el $MAIN
 tar -cvzf $OUT/el-darwin-amd64.tar.gz -C $BUILD .
 
 # echo "windows"
-# env GOARCH=amd64 GOOS=windows go build -ldflags="-X 'el/internal/constants.Version=$1'" -o $BUILD/el ./cmd/
-# tar -cvzf $OUT/el-darwin-amd64.tar.gz -C $BUILD .
+# env GOARCH=amd64 GOOS=windows go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el $MAIN
+# tar -cvzf $OUT/el-windows-amd64.tar.gz -C $BUILD .
 
 echo "Cleaning up..."
 rm -r $BUILD
