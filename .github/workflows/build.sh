1482526701175b3393f7fa503021e600299cb32e
just-hms <alessandroversari1@gmail.com>
2024-02-28 22:47:06 +0100

    fix: go version

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -9,24 +9,22 @@ VERSION=$1
 BUILD="./build"
 OUT="./out"
 
-echo "Creating the ouput folder..."
+echo "Creating the build folders..."
 mkdir -p $OUT
 mkdir -p $BUILD
 
 echo "Copying shell files to build folder..."
-cp -r ./shell/ $BUILD
+cp ./shell/* $BUILD
 
 echo "Building..."
 
 echo "linux"
 env GOARCH=amd64 GOOS=linux go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el ./cmd/
 tar -cvzf $OUT/el-linux-amd64.tar.gz $BUILD/*
-rm $BUILD/el
 
 echo "darwin"
-env GOARCH=amd64 GOOS=darwin go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $OUT/el ./cmd/
-tar -cvzf $OUT/el-linux-amd64.tar.gz $BUILD/*
-rm $BUILD/el
+env GOARCH=amd64 GOOS=darwin go build -ldflags="-X 'el/internal/constants.Version=$VERSION'" -o $BUILD/el ./cmd/
+tar -cvzf $OUT/el-darwin-amd64.tar.gz $BUILD/*
 
 # echo "windows"
 # env GOARCH=amd64 GOOS=windows go build -ldflags="-X 'el/internal/constants.Version=$1'" -o ./out/el.exe ./cmd/
