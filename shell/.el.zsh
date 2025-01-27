554ce59fb40f4b7a6db97e56024c1e42d288a9dc
just-hms <alessandroversari1@gmail.com>
2025-01-27 18:42:54 +0100

    edit: zsh and sh setup to handle multiline commands

--- shell/.el.zsh
+++ shell/.el.zsh
@@ -14,9 +14,9 @@ function sugg() {
   # Get the left part of the buffer up to the cursor position
   local left_part=${BUFFER:0:$CURSOR}
 
-  # change the buffer to match the selection    
-  BUFFER=$(el | sort -u | fzf -q "$left_part")
-  
+  # change the buffer to match the selection
+  BUFFER=$(el | sort -u | fzf -q "$left_part" --read0)
+
   # Update the cursor position
   CURSOR=$#BUFFER
 }
